5f7deb1a2377b2fdd46c048e7dd3c68dcbce3c00
SevenPik <108593577+SevenPik@users.noreply.github.com>
2023-03-15 23:14:15 +0100

    Nowy Plik Nowy ja
    zmieniony plik sales z ulepszeniami plus kalkulator norm szachowych w designie

--- Sklep/sales.go
+++ Sklep/sales.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func DoubleRoundFive(val float32) float32 {
+func floatnaint(val float32) float32 {
 	valInt := int64(val * 100)
 	val = float32(valInt) / 100
 	return val
@@ -22,6 +22,7 @@ func Menu(produkt string) (float32, string) {
 		cenaProduktów = cenaProduktów + 3.99
 	default:
 		wybórProduktu = "Nic"
+		break
 	}
 	fmt.Println(cenaProduktów, wybórProduktu)
 	return cenaProduktów, wybórProduktu
@@ -29,7 +30,7 @@ func Menu(produkt string) (float32, string) {
 
 func promocjewbiedrze(promka int, promocja float64) float64 {
 	rand.Seed(time.Now().UnixNano())
-	promka = rand.Intn(100)
+	promka = rand.Intn(50)
 	fmt.Println(promka, "% Promocja (promka)")
 	promocja = 100
 	promocja = float64(promka) / 100
@@ -40,19 +41,24 @@ func promocjewbiedrze(promka int, promocja float64) float64 {
 func main() {
 	var x int
 	var y int
+	fmt.Println("Witaj w Sklepie!", " Oto nasze produkty: Bułka - 3.99 ")
+	fmt.Println("Ile chcesz kupić rzeczy?")
 	fmt.Scanln(&y)
 	var łącznykoszt float32
 	for x = 0; x < y; x++ {
 		fmt.Println("Co chcesz kupić? ")
 		var zachcianka string
 		fmt.Scanln(&zachcianka)
+		if zachcianka == "nic" {
+			break
+		}
 		var menucena float32
 		var menunazwa string
 		bydziałało := promocjewbiedrze(1, 1)
 
 		menucena, menunazwa = Menu(zachcianka)
 		menucena = menucena - menucena*float32(bydziałało)
-		menucena = float32(DoubleRoundFive(menucena))
+		menucena = float32(floatnaint(menucena))
 		fmt.Println(menucena, menunazwa)
 		łącznykoszt = łącznykoszt + menucena
 		fmt.Println(łącznykoszt, "Łączny koszt")
